eac22b4bb6594e9bafc184538182dad16451c41a
Henrik Lilleengen <mail@ithenrik.com>
2017-11-13 23:00:05 +0100

    Add basic API request

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <select>
+      <option v-for="program in programs" :value="program.id">{{program.displayName}}</option>
+    </select>
     <line-example/>
   </div>
 </template>
@@ -16,7 +19,22 @@
     data() {
       return {
         msg: 'Welcome to Your Vue.js App',
+        programs: [],
       };
+    },
+    created() {
+      this.fetchPrograms();
+    },
+    methods: {
+      fetchPrograms() {
+        this.$http.get('https://play.dhis2.org/demo/api/programs.json', {
+          headers: {
+            Authorization: 'Basic a29rZXJpZXQ6T2JsaWcuc29sdXRpb241',
+          },
+        }).then((response) => {
+          this.programs = response.body.programs;
+        });
+      },
     },
   };
 </script>
@@ -26,14 +44,17 @@
   h1, h2 {
     font-weight: normal;
   }
+
   ul {
     list-style-type: none;
     padding: 0;
   }
+
   li {
     display: inline-block;
     margin: 0 10px;
   }
+
   a {
     color: #42b983;
   }
