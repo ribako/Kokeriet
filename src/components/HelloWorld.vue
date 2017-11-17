cdc353af8b61499cc3fc41f3f9a47d4b0659ab49
Andreas <bonsjo@hotmail.com>
2017-11-17 15:19:25 +0100

    Added sliders, updated looks of graph, started working on min, max

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,14 +7,18 @@
     <select v-model="selectedItem">
       <option v-for="item in stockItems" :value="item.id">{{item.displayName}}</option>
     </select>
-    <h1>{{ stockData }}</h1>
-    <line-example :chart-data="datacollection" :options="{responsive: true, maintainAspectRatio: false}"></line-example>
+    <input type="range" min="1" max="100" value="1" class="slider" id="min">
+    <input type="range" min="1" max="100" value="1" class="slider" id="max">
+    <!--<h1>{{ stockData }}</h1>-->
+    <line-example :chart-data="datacollection" :options="options"></line-example>
   </div>
 </template>
 
 <script>
   // import Vue from 'vue';
   import LineExample from './LineChart.jsx';
+
+  // TODO install awesome slider from either https://nightcatsama.github.io/vue-slider-component/example/#QuickStart or somewhere else
 
 
   export default {
@@ -30,14 +34,34 @@
         stockData: {},
         oldItem: null,
         oldOrg: null,
+        oldMin: null,
+        oldMax: null,
+        min: null,
+        max: null,
         selectedItem: null,
         selectedOrg: null,
-        datacollection: {
-          labels: [],
-          datasets: [{
-            label: 'Stuff',
-            data: [],
-          }],
+        datacollection: {},
+        options: {
+          scales: {
+            yAxes: [{
+              ticks: {
+                beginAtZero: true,
+              },
+              gridLines: {
+                display: true,
+              },
+            }],
+            xAxes: [{
+              gridLines: {
+                display: false,
+              },
+            }],
+          },
+          legend: {
+            display: true,
+          },
+          responsive: true,
+          maintainAspectRatio: false,
         },
       };
     },
@@ -71,6 +95,36 @@
       },
       getDataForGraph() {
         if (this.selectedItem !== this.oldItem && this.selectedOrg !== this.oldOrg) {
+          const labels = [];
+          const datasets = [{
+            label: 'Data',
+            borderColor: '#0016bf', // Color of the line
+            backgroundColor: 'rgba(0, 0, 0, 0)', // Makes the fill under the line 100% transparent
+            pointBackgroundColor: 'white',
+            borderWidth: 1,
+            pointBorderColor: '#249EBF',
+            data: [],
+          },
+          {
+            label: 'Min',
+            borderDash: [40, 10],
+            borderColor: '#bf000a',
+            pointRadius: 0,
+            backgroundColor: 'rgba(0, 0, 0, 0)',
+            borderWidth: 1,
+            pointBorderColor: '#6d6e6c',
+            data: [],
+          },
+          {
+            label: 'Max',
+            borderDash: [40, 10],
+            borderColor: '#bf000a',
+            pointRadius: 0,
+            backgroundColor: 'rgba(0, 0, 0, 0)',
+            borderWidth: 1,
+            pointBorderColor: '#6d6e6c',
+            data: [],
+          }];
           this.oldItem = this.selectedItem;
           this.oldOrg = this.selectedOrg;
           this.$http.get(`https://inf5750.dhis2.org/training/api/26/analytics?dimension=dx:${this.selectedItem}`
@@ -87,13 +141,16 @@
                 return (a[1] < b[1]) ? -1 : 1;
               }
               this.stockData.sort(sortFunction);
-              // Should really move the following to a method which isn't called every god damn ms
-              // this.datacollection.labels = [this.stockData[0][1], this.stockData[1][1]]
               this.stockData.forEach((elem) => {
-                this.datacollection.labels.push(elem[1]);
-                this.datacollection.datasets[0].data.push(elem[3]);
+                labels.push(elem[1]);
+                datasets[0].data.push(elem[3]);
+                datasets[1].data.push(50000);
+                datasets[2].data.push(80000);
               });
-              console.log(this.datacollection.labels);
+              this.datacollection = {
+                labels,
+                datasets,
+              };
             });
         }
       },
--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -2,6 +2,7 @@ import { Line, mixins } from 'vue-chartjs';
 
 const { reactiveProp } = mixins;
 
+
 export default {
   extends: Line,
   mixins: [reactiveProp],
